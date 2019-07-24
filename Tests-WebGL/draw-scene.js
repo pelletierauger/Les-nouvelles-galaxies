ec537f29314ddbfaf8dcbfaf5b91aaeb384c8854
Guillaume Pelletier-Auger <pelletierauger@gmail.com>
2019-07-24 02:59:03 -0400

    Create a swaying spiral

--- Tests-WebGL/draw-scene.js
+++ Tests-WebGL/draw-scene.js
@@ -33,11 +33,11 @@ drawDots = function() {
     let x = 1;
     let y = 1;
     for (let i = 0; i < 30000; i += 1) {
-        x = sin(tan(i * 25 + t) + i * t * 0.000000001) * i * 0.00005;
-        y = cos(tan(i * 25 + t) + i * t * 0.000000001) * i * 0.00015;
-//         x *= cos(fx * fy * 0.001) * sin(x + t * 20);
-//         y *= cos(fx * fy * 0.001) * cos(x + t * 20);
-        x += sin(fx * 0.18) * 5;
+        x = sin(tan(i * 25 + t) + i * t * 0.00000005) * i * 0.00005;
+        y = cos(tan(i * 25 + t) + i * t * 0.00000005) * i * 0.00015;
+        x *= cos(fx * fy * 0.001) * sin(x + t * 40);
+        y *= cos(fx * fy * 0.001) * cos(x + t * 20);
+        x += cos(fx * 0.18) * 5;
         y += sin(fy * 0.18) * 5;
         fx = x;
         fy = y;
@@ -45,7 +45,7 @@ drawDots = function() {
 //         y += (Math.random() - 0.5) * 0.00005;
         x += xOffset * 0.25;
         y += yOffset * 0.25;
-        vertices.push(x * 1.5 * 0.235, y * 0.8 * 0.235 - 0.75, 0.0);
+        vertices.push(x * 1 - 3.8, y * 0.8 * 0.21 - 0.5, 0.0);
     }
     // Create an empty buffer object to store the vertex buffer
     // var vertex_buffer = gl.createBuffer();
