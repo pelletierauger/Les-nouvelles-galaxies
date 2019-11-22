b216363cca962adac36f9efb6ec71fc613eba20b
Guillaume Pelletier-Auger <pelletierauger@gmail.com>
2019-11-22 17:13:43 -0500

    Add the gold dots shader in shaders.js

--- Bruit-vert/shaders.js
+++ Bruit-vert/shaders.js
@@ -750,9 +750,70 @@ void main() {
     // gl_FragColor = gl_FragColor.grba;
     gl_FragColor.rgb *= 1.;
         gl_FragColor = gl_FragColor.brga;
-        // gl_FragColor.r *= 3.;
+        gl_FragColor.r *= 0.5;
+        gl_FragColor.b *= 1.25;
         // gl_FragColor = gl_FragColor.grra;
 }
 // endGLSL
 `;
 pulsarFog.init();
+
+
+
+
+let gold = new ShaderProgram("gold");
+
+gold.vertText = `
+    // beginGLSL
+    attribute vec4 coordinates;
+    varying vec2 myposition;
+    varying vec2 center;
+    varying float alph;
+    void main(void) {
+        gl_Position = vec4(coordinates.x, coordinates.y, 0.0, 1.0);
+        center = vec2(gl_Position.x, gl_Position.y);
+        center = 512.0 + center * 512.0;
+        myposition = vec2(gl_Position.x, gl_Position.y);
+        alph = coordinates.w;
+        gl_PointSize = 9. + coordinates.z / ((6.0 + alph) * 0.25);
+        // gl_PointSize = 25.0 + cos((coordinates.x + coordinates.y) * 4000000.) * 5.;
+        // gl_PointSize = coordinates.z / (alph * (sin(myposition.x * myposition.y * 1.) * 3. + 0.5));
+    }
+    // endGLSL
+`;
+gold.fragText = `
+    // beginGLSL
+    precision mediump float;
+    varying vec2 myposition;
+    varying vec2 center;
+    varying float alph;
+    float rand(vec2 co){
+        return fract(sin(dot(co.xy ,vec2(12.9898,78.233))) * 43758.5453 * (2.0 + sin(co.x)));
+    }
+    void main(void) {
+        // vec2 uv = gl_PointCoord.xy / vec2(1600, 1600);
+        // float d = length(uv - center);
+        // vec2 pos = myposition;
+        vec2 uv = gl_FragCoord.xy / vec2(2560, 1600);
+        // uv.x = uv.x + 1.0;
+        uv = uv * 2.0;
+        uv = uv + 0.5;
+        // uv = uv * 1.0;
+        float ALPHA = 0.75;
+        vec2 pos = gl_PointCoord - vec2(0.5, 0.5);
+                float dist_squared = dot(pos, pos);
+        float alpha;
+        if (dist_squared < 0.25) {
+            alpha = ALPHA;
+        } else {
+            alpha = 0.0;
+        }
+        alpha = smoothstep(0.015 / (0.9 + alph), 0.000125, dist_squared) * 0.49;
+        float alpha2 = smoothstep(0.15 / (0.9 + alph), 0.000125, dist_squared) * 0.49;
+        float rando = rand(pos);
+        // gl_FragColor = vec4(1.0, (1.0 - dist_squared * 40.) * 0.6, 0.0, alpha + ((0.12 - dist_squared) * 4.) - (rando * 0.2));
+        gl_FragColor = vec4(0.3 + alpha2 * 40.0, 0.1 + alpha * 1.0, 0.25 + alpha * 0., ((3. - dist_squared * 24.0 * (0.25 + alph) - (rando * 1.1)) * 0.045 + alpha)) * 1.5;        
+    }
+    // endGLSL
+`;
+gold.init();
