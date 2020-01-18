e68aab7f5ec94141a307714ea8af334b3d814b76
Guillaume Pelletier-Auger <pelletierauger@gmail.com>
2020-01-18 07:11:01 -0500

    Fountain

--- Vert/shaders.js
+++ Vert/shaders.js
@@ -961,12 +961,12 @@ float map(float value, float min1, float max1, float min2, float max2) {
         float t = time * 1e-2;
         float osc = map(sin(t * 16e-1), -1., 1., 0.05, 4.005);
         float i = vertexID;
-        float x = cos(i * i * i * 1e-10 * cos(i * 12e1)) * i * 1e-5;
-        float y = sin(i * i * i * x * 1e-10 * sin(i * 12e1)) * i * 1e-5;
+        float x = cos(i * i * i * i * 1e-16 * cos(i * 12e2)) * i * 1e-5;
+        float y = sin(i * i * i * i * x * 1e-16 * sin(i * 12e2)) * i * 1e-5;
 //         x = tan(x * 0.25) + x * 0.15;
 //         y = tan(y * 0.25) + y * 0.15;
-        x = tan((x + y));
-        y = tan((x + y) * sin(t * 1e-1) * 5.);
+//         x = tan((x + y));
+        y = tan((x + y * y * y + x * x - x) + y * 2e9 * sin(t * 1e-3));
         x *= 0.25 * 2.5;
         y *= 0.25 * 2.5;
         x += cos(t * 16e-1) * i * 0.0000035;
