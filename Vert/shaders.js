ecad8a76f390ad17d99e1b9397d6880353cee2ce
Guillaume Pelletier-Auger <pelletierauger@gmail.com>
2020-12-09 01:41:33 -0500

    Mythical oaks

--- Vert/shaders.js
+++ Vert/shaders.js
@@ -958,20 +958,21 @@ float map(float value, float min1, float max1, float min2, float max2) {
 }
 // 
     void main(void) {
-        float t = time * 1e-2;
+        float t = time * 2e-3;
         float osc = map(sin(t * 16e-1), -1., 1., 0.05, 4.005);
         float i = vertexID * 1e-1;
-        // float x = cos(i * 0.5e-5) * 8e-1;
-        // float y = sin(i * 0.5e-5) * 8e-1;
-        float x = tan(cos(i * 0.001 * sin(t)) * 260. * tan(i * 4e16)) * 4.;
-        float y = tan(sin(i * 0.001 * sin(t)) * 260. * tan(i * 4e16)) * 4.;
+        float x = cos(tan(i * 1.5e-7) * 5e3 * i) * i * i * 8e-7;
+        float y = sin(tan(i * 1.5e-7) * 5e3 * i) * i * i * 8e-7;
+        x += cos(i * 690. * tan(i * 2e-1)) * 1000.5;
+        y += sin(i * 690. * tan(i * 2e-1)) * 1000.5;
 //         x *= 0.25 * 44.5;
 //         y *= 0.25 * 44.5;
-        x += cos(t * 0.25e1) * i * 0.0005;
-        y += sin(t * 0.25e1) * i * 0.0005;
-                x -= 1.;
-        x *= 0.05;
-        y *= 0.05;
+        x += cos(t * 0.75e1) * i * 0.005;
+        y += sin(t * 0.75e1) * i * 0.005;
+        x += 1000. * cos(t * 2.);
+        y += 1000. * sin(t * 2.);
+        x *= 1.25e-3;
+        y *= 1.25e-3;
 //         float x = cos(i) * i * 1e-5 * 2.;
 //         float y = sin(i) * i * 1e-5 * 2.;
         gl_Position = vec4(x * 0.6, y, 0.0, 1.0);
@@ -987,6 +988,7 @@ float map(float value, float min1, float max1, float min2, float max2) {
 `;
 newFlickeringVert.fragText = `
     // beginGLSL
+   // beginGLSL
     precision mediump float;
 //     varying vec2 myposition;
 //     varying vec2 center;
@@ -1015,10 +1017,12 @@ newFlickeringVert.fragText = `
         alpha = smoothstep(0.05 / (0.9 + alph), 0.000125, dist_squared) * 0.49;
         float rando = rand(pos);
         // gl_FragColor = vec4(1.0, (1.0 - dist_squared * 40.) * 0.6, 0.0, alpha + ((0.12 - dist_squared) * 4.) - (rando * 0.2));
-        gl_FragColor = vec4(1.0, 0.4 - dist_squared, 2.0 + alpha * 120., ((3. - dist_squared * 24.0 * (0.25 + alph) - (rando * 1.1)) * 0.045 + alpha)) * 0.25;
-        // gl_FragColor = gl_FragColor.brba;
+        gl_FragColor = vec4(1.0, 0.4 - dist_squared, 0.0 + alpha * 120., ((3. - dist_squared * 24.0 * (0.25 + alph) - (rando * 1.1)) * 0.045 + alpha)) * 1.25;
+//         gl_FragColor = gl_FragColor.gbga;
+        gl_FragColor.rgb = vec3(1.0) - gl_FragColor.rgb;
+             gl_FragColor = gl_FragColor.grba;
 //         gl_FragColor.g *= 0.525;
-//         gl_FragColor.b *= 0.0125;
+        gl_FragColor.b *= 0.5;
         
     }
     // endGLSL
