e911448ee8aaaaf5aae52cd0e9c0f0e96f3ec956
Guillaume Pelletier-Auger <pelletierauger@gmail.com>
2021-06-26 19:41:36 -0400

    Floating torn muscles

--- Vert/shaders.js
+++ Vert/shaders.js
@@ -967,8 +967,8 @@ float map(float value, float min1, float max1, float min2, float max2) {
         float i = vertexID * 1e-1;
         float x = cos(pow(i, sin(i * 2e4)) * 20e1 * cos(t)) * i * 2e-2;
         float y = sin(pow(i, sin(i * 2e4)) * 20e1 * sin(t)) * i * 2e-2;
-        x += tan(cos(i * 3. * tan(x * 1e-3)) + t * 1e2) * 500.5;
-        y += tan(sin(i * 3. * tan(x * 1e-3)) + t * 1e2) * 500.5;
+        x += tan(x * 1e-3 + cos(i * 1e1 * tan(x)) + t * 1e2) * 500.5;
+        y += tan(x * 1e-3 + sin(i * 1e1 * tan(x)) + t * 1e2) * 500.5;
 //         x *= 0.25 * 44.5;
 //         y *= 0.25 * 44.5;
         // x += cos(t * 2.75e1) * i * 0.005;
