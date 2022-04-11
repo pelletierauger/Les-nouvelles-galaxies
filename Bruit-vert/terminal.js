043876184ffee16e8b7f9ce612053b70c428bdbd
Guillaume Pelletier-Auger <pelletierauger@gmail.com>
2022-04-11 19:35:28 -0400

    Work on the virtual terminal

---
+++ Bruit-vert/terminal.js
@@ -0,0 +1,91 @@
+drawSwirl = function(selectedProgram) {
+    vertices = [];
+    let num = 150;
+    for (let i = 0; i < num; i++) {
+        let x = Math.cos(i) * i * 1e-2;
+        let y = Math.sin(i) * i * 1e-2;
+        vertices.push(x * (9 / 16), y, 524.0, 1);
+    }
+    // Create an empty buffer object to store the vertex buffer
+    // var vertex_buffer = gl.createBuffer();
+    //Bind appropriate array buffer to it
+    // gl.bindBuffer(gl.ARRAY_BUFFER, vertex_buffer);
+    // Pass the vertex data to the buffer
+    // Unbind the buffer
+    gl.bindBuffer(gl.ARRAY_BUFFER, null);
+    /*======== Associating shaders to buffer objects ========*/
+    // Bind vertex buffer object
+    gl.bindBuffer(gl.ARRAY_BUFFER, dotsVBuf);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+    // Get the attribute location
+    var coord = gl.getAttribLocation(selectedProgram, "coordinates");
+    // Point an attribute to the currently bound VBO
+    gl.vertexAttribPointer(coord, 4, gl.FLOAT, false, 0, 0);
+    // Enable the attribute
+    gl.enableVertexAttribArray(coord);
+    /*============= Drawing the primitive ===============*/
+    // // Clear the canvas
+    // gl.clearColor(0.5, 0.5, 0.5, 0.9);
+    // Clear the color buffer bit
+    // gl.clear(gl.COLOR_BUFFER_BIT);
+    // Draw the triangle
+    let dotsToDraw = Math.floor(map(drawCount, 0, 2400 - 672, 60000, 0));
+    gl.drawArrays(gl.POINTS, 0, num);
+}
+
+
+let roundedSquare = new ShaderProgram("rounded-square");
+
+roundedSquare.vertText = `
+    // beginGLSL
+    attribute vec4 coordinates;
+    varying vec2 myposition;
+    varying vec2 center;
+    varying float alph;
+    varying float size;
+    void main(void) {
+        gl_Position = vec4(coordinates.x, coordinates.y, 0.0, 1.0);
+        center = vec2(gl_Position.x, gl_Position.y);
+        center = 512.0 + center * 512.0;
+        myposition = vec2(gl_Position.x, gl_Position.y);
+        alph = coordinates.w;
+        gl_PointSize = coordinates.z;
+        size = gl_PointSize;
+        // gl_PointSize = 25.0 + cos((coordinates.x + coordinates.y) * 4000000.) * 5.;
+        // gl_PointSize = coordinates.z / (alph * (sin(myposition.x * myposition.y * 1.) * 3. + 0.5));
+    }
+    // endGLSL
+`;
+roundedSquare.fragText = `
+    // beginGLSL
+    precision mediump float;
+    varying vec2 myposition;
+    varying vec2 center;
+    varying float alph;
+    varying float size;
+    float rand(vec2 co){
+        return fract(sin(dot(co.xy ,vec2(12.9898,78.233))) * 43758.5453 * (2.0 + sin(co.x)));
+    }
+    float roundedRectangleFlicker (vec2 uv, vec2 pos, vec2 size, float radius, float thickness) {
+        // vec2 uv = gl_PointCoord.xy;
+        float time = 0.0;
+        float w = 0.15 + (sin(time * 1e-2 * tan(time * 2e-2)) + 1.0) * 0.25;
+        float d = length(max(abs(uv - pos), size * 0.5) - size * 0.5) * w - radius * 0.01;
+        return smoothstep(1.99 + ((sin(time * 10. * tan(time * 1e1)) + 1.0) * 0.5), 0.11, d * 10. / thickness * 5.0 * 0.125);
+    }
+    float roundedRectangle (vec2 uv, vec2 pos, vec2 size, float radius, float thickness) {
+        float d = length(max(abs(uv - pos), size) - size) - radius;
+        return smoothstep(0.66, 0.33, d / thickness * 5.0);
+    }
+    void main(void) {
+         float resolution = 1.0;
+         vec2 screenSize = vec2(2560.0, 1440.0) * resolution;
+         vec2 uv = gl_PointCoord.xy;
+        uv = uv * 2. - 1.;
+        float color = roundedRectangleFlicker(uv, vec2(0.0, 0.0), vec2(1.0), 1.0, 0.1);
+        float rando = rand(uv);
+        gl_FragColor = vec4(vec3(color), 1.0);
+    }
+    // endGLSL
+`;
+roundedSquare.init();
