1e3d330aaaa673d57d99a144364c2d0b72e9510f
Guillaume Pelletier-Auger <pelletierauger@gmail.com>
2022-04-12 17:52:31 -0400

    Add more characters

--- Bruit-vert/sketch.js
+++ Bruit-vert/sketch.js
@@ -237,21 +237,21 @@ draw = function() {
 
 keyPressed = function() {
     if (keysActive) {
-        if (keyCode === 32) {
-            if (looping) {
-                noLoop();
-                looping = false;
-            } else {
-                loop();
-                looping = true;
-            }
-        }
-        if (key == 'r' || key == 'R') {
-            window.location.reload();
-        }
-        if (key == 'm' || key == 'M') {
-            redraw();
-        }
+        // if (keyCode === 32) {
+        //     if (looping) {
+        //         noLoop();
+        //         looping = false;
+        //     } else {
+        //         loop();
+        //         looping = true;
+        //     }
+        // }
+        // if (key == 'r' || key == 'R') {
+        //     window.location.reload();
+        // }
+        // if (key == 'm' || key == 'M') {
+        //     redraw();
+        // }
         // if (key == 'a' || key == 'A') {
         //     logJavaScriptConsole("yur!");
         // }
@@ -273,3 +273,14 @@ keyPressed = function() {
         }
     }
 }
+
+
+tl = function() {
+                if (looping) {
+                noLoop();
+                looping = false;
+            } else {
+                loop();
+                looping = true;
+            }
+};
