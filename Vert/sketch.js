2727cd06f34d6e7568a3d5af570a92918264e2b4
Guillaume Pelletier-Auger <pelletierauger@gmail.com>
2022-05-07 19:22:37 -0400

    Stop drawing pulsarFog in verfun-dark

--- Vert/sketch.js
+++ Vert/sketch.js
@@ -144,16 +144,15 @@ function clearSelection() {
 }
 
 draw = function() {
-    gl.clear(gl.COLOR_BUFFER_BIT);
     // We bind the framebuffer...
     bindFrameBuffer(texture, framebuf);
     gl.viewport(0, 0, 1280, 720);
-// 
+    gl.clear(gl.COLOR_BUFFER_BIT);
     // draw the scene, presumably on a framebuffer
-    let currentProgram = getProgram("pulsar-fog");
-    gl.useProgram(currentProgram);
-    drawBG(currentProgram);
-    currentProgram = getProgram("new-flickering-dots-vert");
+    // let currentProgram = getProgram("pulsar-fog");
+    // gl.useProgram(currentProgram);
+    // drawBG(currentProgram);
+    let currentProgram = getProgram("new-flickering-dots-vert");
     gl.useProgram(currentProgram);
     drawAlligatorQuietVert(currentProgram);
 // 
@@ -313,7 +312,7 @@ draw = function() {
 // 
     gl.bindTexture(gl.TEXTURE_2D, texture);
 // 
-    gl.clearColor(0.5, 0.5, 0.5, 1); // clear to white
+    gl.clearColor(0, 0, 0, 1); // clear to white
 // 
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 // 
